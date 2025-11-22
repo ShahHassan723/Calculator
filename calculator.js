686695fad8d59f252352db4c4ef56b8beeca984f
ShahHassan723 <shahhssan0@gmail.com>
2025-11-22 01:57:41 -0800

    js poroject

---
+++ calculator.js
@@ -0,0 +1,32 @@
+function add(){
+var FirstValue = prompt("Enter The First value");
+var SecondValue = prompt("Enter The Second Value");
+var Result = Number(FirstValue) + Number (SecondValue);
+alert(Result);
+console .log(Result)
+}
+
+function divi(){
+var FirstValue = prompt("Enter The First value");
+var SecondValue = prompt("Enter The Second Value");
+var Result = Number(FirstValue) /  Number (SecondValue);
+alert(Result);
+console .log(Result)
+}
+
+function mul(){
+var FirstValue = prompt("Enter The First value");
+var SecondValue = prompt("Enter The Second Value");
+var Result = Number(FirstValue) *  Number (SecondValue);
+alert(Result);
+console .log(Result)
+}
+
+function sub(){
+var FirstValue = prompt("Enter The First value");
+var SecondValue = prompt("Enter The Second Value");
+var Result = Number(FirstValue) -  Number (SecondValue);
+alert(Result);
+console .log(Result)
+}
+
